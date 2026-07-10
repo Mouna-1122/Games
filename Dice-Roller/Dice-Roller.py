c295bf285e30e2d85f7fab8a063b2d61bff980f4
Mouna Mahesh <mmouna7183@gmail.com>
2026-07-10 19:59:30 +0530

    Add files via upload

---
+++ Dice-Roller/Dice-Roller.py
@@ -0,0 +1,15 @@
+import random
+
+print("🎲 Welcome to the Dice Roller Game!")
+
+while True:
+    input("\nPress Enter to roll the dice...")
+
+    dice = random.randint(1, 6)
+    print(f"You rolled a {dice}!")
+
+    choice = input("\nRoll again? (y/n): ").lower()
+
+    if choice != 'y':
+        print("\nThanks for playing! 👋")
+        break
